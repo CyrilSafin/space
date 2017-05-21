69a20919e74c2000df7c53aef0cc0ebe07062593
Cyril Safin <kirillsafin@nlstar.com>
2017-05-22 00:30:57 +0400

    First

---
+++ Space/Space/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetBufferSize(80,25);
+
+            List<cPoint> HorizontalLine = new List<cPoint>();
+            HorizontalLine.Add(p);
+
+            for (int i = 0; i < 80;i++ )
+            { 
+                cPoint p1 = new cPoint(i, 0, '+');
+                p1.DrawPoint();
+            }
+            
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Space/Space/cPoint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Space
+{
+    class cPoint
+    {
+        int x;
+        int y;
+        char sym;
+
+        public cPoint (int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+        public void DrawPoint()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
